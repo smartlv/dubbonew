9cf6bccb12af3f4226d41180b59bfe24b1697015
smartlv <smart.lv@bluestonehk.com>
2016-11-30 17:02:39 +0800

    去掉dubbo一些不必要的扩展，并升级spring，以及jdk，以及日志log4j2

--- dubbo-common/src/test/java/com/alibaba/dubbo/common/utils/AtomicPositiveIntegerTest.java
+++ dubbo-common/src/test/java/com/alibaba/dubbo/common/utils/AtomicPositiveIntegerTest.java
@@ -15,18 +15,17 @@
  */
 package com.alibaba.dubbo.common.utils;
 
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.junit.matchers.JUnitMatchers.containsString;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 /**
  * @author ding.lid
  */
-public class AtomicPositiveIntegerTest {
+public class AtomicPositiveIntegerTest
+{
     AtomicPositiveInteger i1 = new AtomicPositiveInteger();
 
     AtomicPositiveInteger i2 = new AtomicPositiveInteger(127);
@@ -34,28 +33,33 @@ public class AtomicPositiveIntegerTest {
     AtomicPositiveInteger i3 = new AtomicPositiveInteger(Integer.MAX_VALUE);
 
     @Test
-    public void test_get() throws Exception {
+    public void test_get() throws Exception
+    {
         assertEquals(0, i1.get());
         assertEquals(127, i2.get());
         assertEquals(Integer.MAX_VALUE, i3.get());
     }
 
     @Test
-    public void test_set() throws Exception {
+    public void test_set() throws Exception
+    {
         i1.set(100);
         assertEquals(100, i1.get());
 
-        try {
+        try
+        {
             i1.set(-1);
             fail();
-        } catch (IllegalArgumentException expected) {
-            assertThat(expected.getMessage(),
-                    allOf(containsString("new value"), containsString("< 0")));
+        }
+        catch (IllegalArgumentException expected)
+        {
+            assertThat(expected.getMessage(), allOf(containsString("new value"), containsString("< 0")));
         }
     }
 
     @Test
-    public void test_getAndIncrement() throws Exception {
+    public void test_getAndIncrement() throws Exception
+    {
         int get = i1.getAndIncrement();
         assertEquals(0, get);
         assertEquals(1, i1.get());
@@ -70,7 +74,8 @@ public class AtomicPositiveIntegerTest {
     }
 
     @Test
-    public void test_getAndDecrement() throws Exception {
+    public void test_getAndDecrement() throws Exception
+    {
         int get = i1.getAndDecrement();
         assertEquals(0, get);
         assertEquals(Integer.MAX_VALUE, i1.get());
@@ -85,7 +90,8 @@ public class AtomicPositiveIntegerTest {
     }
 
     @Test
-    public void test_incrementAndGet() throws Exception {
+    public void test_incrementAndGet() throws Exception
+    {
         int get = i1.incrementAndGet();
         assertEquals(1, get);
         assertEquals(1, i1.get());
@@ -100,7 +106,8 @@ public class AtomicPositiveIntegerTest {
     }
 
     @Test
-    public void test_decrementAndGet() throws Exception {
+    public void test_decrementAndGet() throws Exception
+    {
         int get = i1.decrementAndGet();
         assertEquals(Integer.MAX_VALUE, get);
         assertEquals(Integer.MAX_VALUE, i1.get());
@@ -115,21 +122,25 @@ public class AtomicPositiveIntegerTest {
     }
 
     @Test
-    public void test_getAndSet() throws Exception {
+    public void test_getAndSet() throws Exception
+    {
         int get = i1.getAndSet(100);
         assertEquals(0, get);
         assertEquals(100, i1.get());
 
-        try {
+        try
+        {
             i1.getAndSet(-1);
-        } catch (IllegalArgumentException expected) {
-            assertThat(expected.getMessage(),
-                    allOf(containsString("new value"), containsString("< 0")));
+        }
+        catch (IllegalArgumentException expected)
+        {
+            assertThat(expected.getMessage(), allOf(containsString("new value"), containsString("< 0")));
         }
     }
 
     @Test
-    public void test_getAndAnd() throws Exception {
+    public void test_getAndAnd() throws Exception
+    {
         int get = i1.getAndAdd(3);
         assertEquals(0, get);
         assertEquals(3, i1.get());
@@ -143,9 +154,9 @@ public class AtomicPositiveIntegerTest {
         assertEquals(2, i3.get());
     }
 
-
     @Test
-    public void test_addAndGet() throws Exception {
+    public void test_addAndGet() throws Exception
+    {
         int get = i1.addAndGet(3);
         assertEquals(3, get);
         assertEquals(3, i1.get());
@@ -160,8 +171,9 @@ public class AtomicPositiveIntegerTest {
     }
 
     @Test
-    public void test_equals() {
-        assertEquals(new AtomicPositiveInteger(), new AtomicPositiveInteger());
-        assertEquals(new AtomicPositiveInteger(1), new AtomicPositiveInteger(1));
+    public void test_equals()
+    {
+        assertEquals(new AtomicPositiveInteger().get(), new AtomicPositiveInteger().get());
+        assertEquals(new AtomicPositiveInteger(1).get(), new AtomicPositiveInteger(1).get());
     }
 }
--- dubbo-simple/dubbo-monitor-simple/src/main/java/com/alibaba/dubbo/monitor/simple/CountUtils.java
+++ dubbo-simple/dubbo-monitor-simple/src/main/java/com/alibaba/dubbo/monitor/simple/CountUtils.java
@@ -15,79 +15,98 @@
  */
 package com.alibaba.dubbo.monitor.simple;
 
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-import com.alibaba.dubbo.common.logger.Logger;
-import com.alibaba.dubbo.common.logger.LoggerFactory;
-
 /**
  * CountUtils
  * 
  * @author william.liangf
  */
-public class CountUtils {
+public class CountUtils
+{
 
     private static final Logger logger = LoggerFactory.getLogger(CountUtils.class);
 
     private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
-    
+
     private static final int SUM = 0;
-    
+
     private static final int MAX = 1;
-    
+
     private static final int AVG = 2;
-    
-    public static long sum(File file) {
+
+    public static long sum(File file)
+    {
         return calc(file, SUM);
     }
 
-    public static long max(File file) {
+    public static long max(File file)
+    {
         return calc(file, MAX);
     }
 
-    public static long avg(File file) {
+    public static long avg(File file)
+    {
         return calc(file, AVG);
     }
-    
-    private static long calc(File file, int op) {
-        if (file.exists()) {
-            try {
+
+    private static long calc(File file, int op)
+    {
+        if (file.exists())
+        {
+            try
+            {
                 BufferedReader reader = new BufferedReader(new FileReader(file));
-                try {
+                try
+                {
                     int times = 0;
                     int count = 0;
                     String line;
-                    while ((line = reader.readLine()) != null) {
+                    while ((line = reader.readLine()) != null)
+                    {
                         int i = line.indexOf(" ");
-                        if (i > 0) {
+                        if (i > 0)
+                        {
                             line = line.substring(i + 1).trim();
-                            if (NUMBER_PATTERN.matcher(line).matches()) {
+                            if (NUMBER_PATTERN.matcher(line).matches())
+                            {
                                 int value = Integer.parseInt(line);
-                                times ++;
-                                if (op == MAX) {
-                                    count = Math.max(count, value);
-                                } else {
+                                times++;
+                                if (op == MAX)
+                                {
+                                    count += Math.max(count, value);
+                                }
+                                else
+                                {
                                     count += value;
                                 }
                             }
                         }
                     }
-                    if (op == AVG) {
+                    if (op == AVG)
+                    {
                         return count / times;
                     }
                     return count;
-                } finally {
+                }
+                finally
+                {
                     reader.close();
                 }
-            } catch (IOException e) {
+            }
+            catch (IOException e)
+            {
                 logger.warn(e.getMessage(), e);
             }
         }
         return 0;
     }
-    
+
 }
