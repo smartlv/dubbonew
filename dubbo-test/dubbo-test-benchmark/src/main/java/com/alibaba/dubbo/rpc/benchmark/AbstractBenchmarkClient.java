5ed40f2850b9d11c05d5e4939fd6e2f16fd7bb15
smartlv <smartlv@mimengstock.com>
2017-12-26 15:14:38 +0800

    dubbo2.6.2 netty4

--- dubbo-test/dubbo-test-benchmark/src/main/java/com/alibaba/dubbo/rpc/benchmark/AbstractBenchmarkClient.java
+++ dubbo-test/dubbo-test-benchmark/src/main/java/com/alibaba/dubbo/rpc/benchmark/AbstractBenchmarkClient.java
@@ -1,26 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.rpc.benchmark;
+
+import com.alibaba.dubbo.common.utils.ConfigUtils;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-
-import com.alibaba.dubbo.common.utils.ConfigUtils;
 
 /**
  * Abstract benchmark client,test for difference scenes Usage: -Dwrite.statistics=false BenchmarkClient serverIP
  * serverPort concurrents timeout codectype requestSize runtime(seconds) clientNums
- * 
- * @author <a href="mailto:bluedavy@gmail.com">bluedavy</a>
  */
-public abstract class AbstractBenchmarkClient
-{
-    // 线程不安全SimpleDateFormat
-    private static final ThreadLocal<SimpleDateFormat> dateFormat = ThreadLocal
-            .withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+public abstract class AbstractBenchmarkClient {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     private static long maxTPS = 0;
 
@@ -65,8 +83,7 @@ public abstract class AbstractBenchmarkClient
 
     Properties properties = ConfigUtils.getProperties();
 
-    public void run(String[] args) throws Exception
-    {
+    public void run(String[] args) throws Exception {
 
         final String serverIP = properties.getProperty("serverip");
         final int serverPort = Integer.parseInt(properties.getProperty("serverport"));
@@ -81,42 +98,35 @@ public abstract class AbstractBenchmarkClient
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(currentDate);
         calendar.add(Calendar.SECOND, runtime);
-
-        StringBuilder startInfo = new StringBuilder(dateFormat.get().format(currentDate));
+        StringBuilder startInfo = new StringBuilder(dateFormat.format(currentDate));
         startInfo.append(" ready to start client benchmark,server is ");
         startInfo.append(serverIP).append(":").append(serverPort);
         startInfo.append(",concurrents is: ").append(concurrents);
         startInfo.append(",clientNums is: ").append(clientNums);
         startInfo.append(",timeout is:").append(timeout);
-        startInfo.append(" s,the benchmark will end at:").append(dateFormat.get().format(calendar.getTime()));
+        startInfo.append(" s,the benchmark will end at:").append(dateFormat.format(calendar.getTime()));
         System.out.println(startInfo.toString());
 
-        // 循环栅栏，join
         CyclicBarrier barrier = new CyclicBarrier(concurrents);
-        // 倒数计数器
         CountDownLatch latch = new CountDownLatch(concurrents);
-
-        List<ClientRunnable> runnables = new ArrayList<>();
-        // java虚拟机预热
+        List<ClientRunnable> runnables = new ArrayList<ClientRunnable>();
         // benchmark start after thirty seconds,let java app warm up
         long beginTime = System.nanoTime() / 1000L + 30 * 1000 * 1000L;
-        for (int i = 0; i < concurrents; i++)
-        {
+        for (int i = 0; i < concurrents; i++) {
             ClientRunnable runnable = getClientRunnable(serverIP, serverPort, clientNums, timeout, barrier, latch,
                     beginTime, endtime);
             runnables.add(runnable);
         }
 
         startRunnables(runnables);
-        // 等着SimpleProcessorBenchmarkClientRunnable 会countdown
+
         latch.await();
 
         // read results & add all
         // key: runtime second range value: Long[2] array Long[0]: execute count Long[1]: response time sum
         Map<String, Long[]> times = new HashMap<String, Long[]>();
         Map<String, Long[]> errorTimes = new HashMap<String, Long[]>();
-        for (ClientRunnable runnable : runnables)
-        {
+        for (ClientRunnable runnable : runnables) {
             List<long[]> results = runnable.getResults();
             long[] responseSpreads = results.get(0);
             below0sum += responseSpreads[0];
@@ -132,11 +142,9 @@ public abstract class AbstractBenchmarkClient
             long[] responseTimes = results.get(2);
             long[] errorTPS = results.get(3);
             long[] errorResponseTimes = results.get(4);
-            for (int i = 0; i < tps.length; i++)
-            {
+            for (int i = 0; i < tps.length; i++) {
                 String key = String.valueOf(i);
-                if (times.containsKey(key))
-                {
+                if (times.containsKey(key)) {
                     Long[] successInfos = times.get(key);
                     Long[] errorInfos = errorTimes.get(key);
                     successInfos[0] += tps[i];
@@ -145,9 +153,7 @@ public abstract class AbstractBenchmarkClient
                     errorInfos[1] += errorResponseTimes[i];
                     times.put(key, successInfos);
                     errorTimes.put(key, errorInfos);
-                }
-                else
-                {
+                } else {
                     Long[] successInfos = new Long[2];
                     successInfos[0] = tps[i];
                     successInfos[1] = responseTimes[i];
@@ -164,52 +170,42 @@ public abstract class AbstractBenchmarkClient
         long ignoreErrorRequest = 0;
         int maxTimeRange = runtime - 30;
         // ignore the last 10 second requests,so tps can count more accurate
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             Long[] values = times.remove(String.valueOf(maxTimeRange - i));
-            if (values != null)
-            {
+            if (values != null) {
                 ignoreRequest += values[0];
             }
             Long[] errorValues = errorTimes.remove(String.valueOf(maxTimeRange - i));
-            if (errorValues != null)
-            {
+            if (errorValues != null) {
                 ignoreErrorRequest += errorValues[0];
             }
         }
 
-        for (Map.Entry<String, Long[]> entry : times.entrySet())
-        {
+        for (Map.Entry<String, Long[]> entry : times.entrySet()) {
             long successRequest = entry.getValue()[0];
             long errorRequest = 0;
-            if (errorTimes.containsKey(entry.getKey()))
-            {
+            if (errorTimes.containsKey(entry.getKey())) {
                 errorRequest = errorTimes.get(entry.getKey())[0];
             }
             allRequestSum += successRequest;
             allResponseTimeSum += entry.getValue()[1];
             allErrorRequestSum += errorRequest;
-            if (errorTimes.containsKey(entry.getKey()))
-            {
+            if (errorTimes.containsKey(entry.getKey())) {
                 allErrorResponseTimeSum += errorTimes.get(entry.getKey())[1];
             }
             long currentRequest = successRequest + errorRequest;
-            if (currentRequest > maxTPS)
-            {
+            if (currentRequest > maxTPS) {
                 maxTPS = currentRequest;
             }
-            if (minTPS == 0 || currentRequest < minTPS)
-            {
+            if (minTPS == 0 || currentRequest < minTPS) {
                 minTPS = currentRequest;
             }
         }
 
         boolean isWriteResult = Boolean.parseBoolean(System.getProperty("write.statistics", "false"));
-        if (isWriteResult)
-        {
+        if (isWriteResult) {
             BufferedWriter writer = new BufferedWriter(new FileWriter("benchmark.all.results"));
-            for (Map.Entry<String, Long[]> entry : times.entrySet())
-            {
+            for (Map.Entry<String, Long[]> entry : times.entrySet()) {
                 writer.write(entry.getKey() + "," + entry.getValue()[0] + "," + entry.getValue()[1] + "\r\n");
             }
             writer.close();
@@ -222,38 +218,33 @@ public abstract class AbstractBenchmarkClient
         System.out.println(" Benchmark Time: " + times.keySet().size());
         long benchmarkRequest = allRequestSum + allErrorRequestSum;
         long allRequest = benchmarkRequest + ignoreRequest + ignoreErrorRequest;
-        System.out.println(" Requests: " + allRequest + " Success: "
-                + (allRequestSum + ignoreRequest) * 100 / allRequest + "% (" + (allRequestSum + ignoreRequest)
-                + ") Error: " + (allErrorRequestSum + ignoreErrorRequest) * 100 / allRequest + "% ("
+        System.out.println(" Requests: " + allRequest + " Success: " + (allRequestSum + ignoreRequest) * 100
+                / allRequest + "% (" + (allRequestSum + ignoreRequest) + ") Error: "
+                + (allErrorRequestSum + ignoreErrorRequest) * 100 / allRequest + "% ("
                 + (allErrorRequestSum + ignoreErrorRequest) + ")");
         System.out.println(" Avg TPS: " + benchmarkRequest / times.keySet().size() + " Max TPS: " + maxTPS
                 + " Min TPS: " + minTPS);
-        System.out.println(
-                " Avg RT: " + (allErrorResponseTimeSum + allResponseTimeSum) / benchmarkRequest / 1000f + "ms");
+        System.out.println(" Avg RT: " + (allErrorResponseTimeSum + allResponseTimeSum) / benchmarkRequest / 1000f
+                + "ms");
         System.out.println(" RT <= 0: " + (below0sum * 100 / allRequest) + "% " + below0sum + "/" + allRequest);
         System.out.println(" RT (0,1]: " + (above0sum * 100 / allRequest) + "% " + above0sum + "/" + allRequest);
         System.out.println(" RT (1,5]: " + (above1sum * 100 / allRequest) + "% " + above1sum + "/" + allRequest);
         System.out.println(" RT (5,10]: " + (above5sum * 100 / allRequest) + "% " + above5sum + "/" + allRequest);
         System.out.println(" RT (10,50]: " + (above10sum * 100 / allRequest) + "% " + above10sum + "/" + allRequest);
         System.out.println(" RT (50,100]: " + (above50sum * 100 / allRequest) + "% " + above50sum + "/" + allRequest);
-        System.out
-                .println(" RT (100,500]: " + (above100sum * 100 / allRequest) + "% " + above100sum + "/" + allRequest);
-        System.out
-                .println(" RT (500,1000]: " + (above500sum * 100 / allRequest) + "% " + above500sum + "/" + allRequest);
+        System.out.println(" RT (100,500]: " + (above100sum * 100 / allRequest) + "% " + above100sum + "/" + allRequest);
+        System.out.println(" RT (500,1000]: " + (above500sum * 100 / allRequest) + "% " + above500sum + "/"
+                + allRequest);
         System.out.println(" RT > 1000: " + (above1000sum * 100 / allRequest) + "% " + above1000sum + "/" + allRequest);
         System.exit(0);
     }
 
     public abstract ClientRunnable getClientRunnable(String targetIP, int targetPort, int clientNums, int rpcTimeout,
-            CyclicBarrier barrier, CountDownLatch latch, long startTime, long endTime)
-            throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException,
-            InvocationTargetException, NoSuchMethodException, ClassNotFoundException;
+                                                     CyclicBarrier barrier, CountDownLatch latch, long startTime,
+                                                     long endTime) throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, ClassNotFoundException;
 
-    // 不用线程池，一个个线程跑
-    protected void startRunnables(List<ClientRunnable> runnables)
-    {
-        for (int i = 0; i < runnables.size(); i++)
-        {
+    protected void startRunnables(List<ClientRunnable> runnables) {
+        for (int i = 0; i < runnables.size(); i++) {
             final ClientRunnable runnable = runnables.get(i);
             Thread thread = new Thread(runnable, "benchmarkclient-" + i);
             thread.start();
--- dubbo-test/dubbo-test-benchmark/src/main/java/com/alibaba/dubbo/rpc/benchmark/SimpleProcessorBenchmarkClientRunnable.java
+++ dubbo-test/dubbo-test-benchmark/src/main/java/com/alibaba/dubbo/rpc/benchmark/SimpleProcessorBenchmarkClientRunnable.java
@@ -1,24 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.dubbo.rpc.benchmark;
 
-/**
- * nfs-rpc Apache License http://code.google.com/p/nfs-rpc (c) 2011
- */
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
  * Simple Processor RPC Benchmark Client Thread
- * 
- * @author <a href="mailto:bluedavy@gmail.com">bluedavy</a>
  */
-public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
-{
+public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable {
 
     private static final Log LOGGER = LogFactory.getLog(SimpleProcessorBenchmarkClientRunnable.class);
 
@@ -64,8 +73,8 @@ public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
     private int maxRange;
 
     public SimpleProcessorBenchmarkClientRunnable(String targetIP, int targetPort, int clientNums, int rpcTimeout,
-            CyclicBarrier barrier, CountDownLatch latch, long startTime, long endTime)
-    {
+                                                  CyclicBarrier barrier, CountDownLatch latch, long startTime,
+                                                  long endTime) {
 
         this.targetIP = targetIP;
         this.targetPort = targetPort;
@@ -81,8 +90,7 @@ public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
         tps = new long[maxRange];
         responseTimes = new long[maxRange];
         // init
-        for (int i = 0; i < maxRange; i++)
-        {
+        for (int i = 0; i < maxRange; i++) {
             errorTPS[i] = 0;
             errorResponseTimes[i] = 0;
             tps[i] = 0;
@@ -90,77 +98,59 @@ public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
         }
     }
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
             barrier.await();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             // IGNORE
         }
         runJavaAndHessian();
         latch.countDown();
     }
 
-    private void runJavaAndHessian()
-    {
-        while (running)
-        {
+    private void runJavaAndHessian() {
+        while (running) {
             Object requestObject = new RequestObject(requestSize);
             long beginTime = System.nanoTime();
-            if (beginTime >= endTime)
-            {
+            if (beginTime >= endTime) {
                 running = false;
                 break;
             }
-            try
-            {
+            try {
                 Object response = null;
                 response = clientFactory.get(targetIP, targetPort, rpcTimeout, clientNums).request(requestObject).get();
                 long currentTime = System.nanoTime();
-                if (beginTime <= startTime)
-                {
+                if (beginTime <= startTime) {
                     continue;
                 }
-
                 long consumeTime = currentTime - beginTime;
                 sumResponseTimeSpread(consumeTime);
                 int range = Integer.parseInt(String.valueOf(beginTime - startTime)) / 1000;
-                if (range >= maxRange)
-                {
-                    System.err.println(
-                            "benchmark range exceeds maxRange,range is: " + range + ",maxRange is: " + maxRange);
+                if (range >= maxRange) {
+                    System.err.println("benchmark range exceeds maxRange,range is: " + range + ",maxRange is: "
+                            + maxRange);
                     continue;
                 }
-                if (((ResponseObject) response).getBytes() != null)
-                {
+                if (((ResponseObject) response).getBytes() != null) {
                     tps[range] = tps[range] + 1;
                     responseTimes[range] = responseTimes[range] + consumeTime;
-                }
-                else
-                {
+                } else {
                     LOGGER.error("server return response is null");
                     errorTPS[range] = errorTPS[range] + 1;
                     errorResponseTimes[range] = errorResponseTimes[range] + consumeTime;
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 LOGGER.error("client.invokeSync error", e);
                 long currentTime = System.nanoTime();
-                if (beginTime <= startTime)
-                {
+                if (beginTime <= startTime) {
                     continue;
                 }
                 long consumeTime = currentTime - beginTime;
                 sumResponseTimeSpread(consumeTime);
                 int range = Integer.parseInt(String.valueOf(beginTime - startTime)) / 1000;
-                if (range >= maxRange)
-                {
-                    System.err.println(
-                            "benchmark range exceeds maxRange,range is: " + range + ",maxRange is: " + maxRange);
+                if (range >= maxRange) {
+                    System.err.println("benchmark range exceeds maxRange,range is: " + range + ",maxRange is: "
+                            + maxRange);
                     continue;
                 }
                 errorTPS[range] = errorTPS[range] + 1;
@@ -169,8 +159,7 @@ public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
         }
     }
 
-    public List<long[]> getResults()
-    {
+    public List<long[]> getResults() {
         List<long[]> results = new ArrayList<long[]>();
         results.add(responseSpreads);
         results.add(tps);
@@ -180,43 +169,25 @@ public class SimpleProcessorBenchmarkClientRunnable implements ClientRunnable
         return results;
     }
 
-    private void sumResponseTimeSpread(long responseTime)
-    {
+    private void sumResponseTimeSpread(long responseTime) {
         responseTime = responseTime / 1000;
-        if (responseTime <= 0)
-        {
+        if (responseTime <= 0) {
             responseSpreads[0] = responseSpreads[0] + 1;
-        }
-        else if (responseTime > 0 && responseTime <= 1)
-        {
+        } else if (responseTime > 0 && responseTime <= 1) {
             responseSpreads[1] = responseSpreads[1] + 1;
-        }
-        else if (responseTime > 1 && responseTime <= 5)
-        {
+        } else if (responseTime > 1 && responseTime <= 5) {
             responseSpreads[2] = responseSpreads[2] + 1;
-        }
-        else if (responseTime > 5 && responseTime <= 10)
-        {
+        } else if (responseTime > 5 && responseTime <= 10) {
             responseSpreads[3] = responseSpreads[3] + 1;
-        }
-        else if (responseTime > 10 && responseTime <= 50)
-        {
+        } else if (responseTime > 10 && responseTime <= 50) {
             responseSpreads[4] = responseSpreads[4] + 1;
-        }
-        else if (responseTime > 50 && responseTime <= 100)
-        {
+        } else if (responseTime > 50 && responseTime <= 100) {
             responseSpreads[5] = responseSpreads[5] + 1;
-        }
-        else if (responseTime > 100 && responseTime <= 500)
-        {
+        } else if (responseTime > 100 && responseTime <= 500) {
             responseSpreads[6] = responseSpreads[6] + 1;
-        }
-        else if (responseTime > 500 && responseTime <= 1000)
-        {
+        } else if (responseTime > 500 && responseTime <= 1000) {
             responseSpreads[7] = responseSpreads[7] + 1;
-        }
-        else if (responseTime > 1000)
-        {
+        } else if (responseTime > 1000) {
             responseSpreads[8] = responseSpreads[8] + 1;
         }
     }
